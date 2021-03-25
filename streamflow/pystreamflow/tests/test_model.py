091ef16c9875707dd8f5e0dc448d5da875ef02ad
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2021-03-25 19:18:26 +0000

    add new forward model

--- streamflow/pystreamflow/tests/test_model.py
+++ streamflow/pystreamflow/tests/test_model.py
@@ -2,8 +2,12 @@
 """
 
 import math
+from importlib import reload
+import sys
 import unittest
+import unittest.mock
 import pystreamflow
+
 
 
 class TestFlux(unittest.TestCase):
@@ -64,3 +68,61 @@ class TestRHS(unittest.TestCase):
         f1 = pystreamflow.ode.ode_rhs(0, *y, 5.0, 1.0, *params1)
         f2 = pystreamflow.ode.ode_rhs(0, *y, 5.0, 1.0, *params2)
         self.assertGreaterEqual(f2[3], f1[3])
+
+
+class TestRiverModel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.input_times = [1, 2, 3, 4, 5, 6, 7]
+        cls.precip = [0, 10, 0, 20, 20, 0, 1]
+        cls.evap = [3, 3.5, 4, 4.5, 5, 5.5, 5.5]
+
+    def test_init(self):
+        # Test initialization
+        pystreamflow.RiverModel(self.input_times, self.precip, self.evap)
+
+    def test_n_parameters(self):
+        m = pystreamflow.RiverModel(self.input_times, self.precip, self.evap)
+        self.assertEqual(m.n_parameters(), 7)
+
+    def test_set_model_data(self):
+        m = pystreamflow.RiverModel(self.input_times, self.precip, self.evap)
+
+        self.assertEqual(m.model_data_times, self.input_times)
+        self.assertEqual(m.rainfall_data, self.precip)
+        self.assertEqual(m.evap_data, self.evap)
+
+    def test_simulate(self):
+        # Test running simulate
+        params = [2.5, 100, 7, 1, -0.5, 60, 3.25]
+        times = [4, 5, 6]
+        m = pystreamflow.RiverModel(self.input_times, self.precip, self.evap)
+        y = m.simulate(params, times)
+        self.assertEqual(len(y), 3)
+
+        m = pystreamflow.RiverModel(
+            self.input_times, self.precip, self.evap, solver='scikit')
+        y = m.simulate(params, times)
+        self.assertEqual(len(y), 3)
+
+    def test_no_data(self):
+        # Test trying to simulate where data has not been provided
+        params = [2.5, 100, 7, 1, -0.5, 60, 3.25]
+        times = [10, 11, 12]
+        m = pystreamflow.RiverModel(self.input_times, self.precip, self.evap)
+
+        with self.assertRaises(RuntimeError) as e:
+            m.simulate(params, times)
+        self.assertTrue('data are not available' in str(e.exception))
+
+    def test_solvers(self):
+        # Test when the scikit solver is not available
+        with unittest.mock.patch.dict(sys.modules):
+            sys.modules['scikits.odes'] = None
+            reload(sys.modules['pystreamflow.model'])
+
+            with self.assertRaises(RuntimeError) as e:
+                pystreamflow.RiverModel(
+                    self.input_times, self.precip, self.evap, solver='scikit')
+            self.assertTrue('scikit solver could not be' in str(e.exception))
