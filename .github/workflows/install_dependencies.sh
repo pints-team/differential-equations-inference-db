e00907ada6d5174d93f1f1380a0ab18a82633f8a
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2021-03-25 20:49:43 +0000

    remove unusual install path

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -9,7 +9,5 @@ wget https://github.com/LLNL/sundials/releases/download/v5.1.0/sundials-5.1.0.ta
 tar xzf sundials-5.1.0.tar.gz
 mkdir build-sundials-5.1.0
 cd build-sundials-5.1.0/
-cmake -DLAPACK_ENABLE=ON -DSUNDIALS_INDEX_SIZE=64 -DCMAKE_INSTALL_PREFIX=/usr/local/ ../sundials-5.1.0/
+cmake -DLAPACK_ENABLE=ON -DSUNDIALS_INDEX_SIZE=64 ../sundials-5.1.0/
 make install
-export SUNDIALS_INST=/usr/local/
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
