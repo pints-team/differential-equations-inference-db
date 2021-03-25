cb7f1e288d6143e5be47dcba6541f16b50899bb2
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2021-03-25 21:08:41 +0000

    use github env variables

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -9,6 +9,7 @@ wget https://github.com/LLNL/sundials/releases/download/v5.1.0/sundials-5.1.0.ta
 tar xzf sundials-5.1.0.tar.gz
 mkdir build-sundials-5.1.0
 cd build-sundials-5.1.0/
-cmake -DLAPACK_ENABLE=ON -DSUNDIALS_INDEX_SIZE=64 ../sundials-5.1.0/
+cmake -DLAPACK_ENABLE=ON -DSUNDIALS_INDEX_SIZE=64 -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE ../sundials-5.1.0/
 make install
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export SUNDIALS_INST=$GITHUB_WORKSPACE
+export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/lib:$LD_LIBRARY_PATH
