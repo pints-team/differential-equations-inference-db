122db4a82b4f4df466054390a0eb5577da67b7da
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2021-03-25 20:31:57 +0000

    switch to sundials v5

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -4,5 +4,10 @@
 apt-get -qq update;
 
 # Install Sundials (for CVODE)
-apt-get install libopenblas-dev liblapack-dev
-apt-get install -y libsundials-dev;
+apt-get install -y libopenblas-dev liblapack-dev
+wget https://github.com/LLNL/sundials/releases/download/v5.1.0/sundials-5.1.0.tar.gz
+tar xzf sundials-5.1.0.tar.gz
+mkdir build-sundials-5.1.0
+cd build-sundials-5.1.0/
+cmake -DLAPACK_ENABLE=ON -DSUNDIALS_INDEX_SIZE=64 -DCMAKE_INSTALL_PREFIX=/usr/local/ ../sundials-5.1.0/
+make install
